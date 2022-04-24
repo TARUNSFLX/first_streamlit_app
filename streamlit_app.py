2cabca0208ca963ff2f4290b2d2ac6dfdbba5da9
tarun9346 <tarun@bordoloi.org>
2022-04-24 13:24:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('Breakfast Favourites')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blue Berry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket - Smoothie')
-streamlit.text('Hard Boiled Free - Range Egg')
+streamlit.text('🥣Omega 3 & Blue Berry Oatmeal')
+streamlit.text('🍨Kale, Spinach & Rocket - Smoothie')
+streamlit.text(🥚'Hard Boiled Free - Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
