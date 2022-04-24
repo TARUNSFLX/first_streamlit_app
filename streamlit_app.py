3df226ed12b43e7e193aee502c12e0e3372a5409
tarun9346 <tarun@bordoloi.org>
2022-04-24 13:41:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.text('🥣Omega 3 & Blue Berry Oatmeal')
 streamlit.text('🍨Kale, Spinach & Rocket - Smoothie')
 streamlit.text('🥚Hard Boiled Free - Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie 🥑🍇')
 
