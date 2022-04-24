969345cced858043664fd1de4dc7e45362041154
tarun9346 <tarun@bordoloi.org>
2022-04-24 12:52:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blue Berry Oatmeal') 
+streamlit.text('Omega 3 & Blue Berry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket - Smoothie')
